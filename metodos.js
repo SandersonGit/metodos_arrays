f6694e256e8cfde946093ba76750f6a46115295c
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-03-26 00:42:41 -0300

    metodo for

---
+++ metodos.js
@@ -0,0 +1,11 @@
+
+// Este código cria uma matriz chamada `redesSociais` que contém três strings representando nomes de redes sociais: "facebook", "instagram" e "twitter". Em seguida, ele usa um loop `for` para iterar sobre cada elemento da matriz e exibir uma mensagem no console para cada rede social, indicando que o usuário tem um perfil nessa rede social. A mensagem exibida inclui o nome da rede social atual, que é acessado usando a variável `i` como índice para acessar cada elemento da matriz durante a iteração.
+
+const redesSociais = ["facebook", "instagram", "twitter"];
+
+for (let i=0; i< redesSociais.length; i++) {
+    console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`);
+    
+}
+console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
