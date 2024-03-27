d20b76bb8bba25a4c8c23136c1b5a76c41b0cc63
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-03-27 18:18:38 -0300

    frist commit

--- metodos.js
+++ metodos.js
@@ -3,9 +3,34 @@
 
 const redesSociais = ["facebook", "instagram", "twitter"];
 
-for (let i=0; i< redesSociais.length; i++) {
+for (let i=0; i < redesSociais.length; i++) {
     console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`);
     
 }
+
+redesSociais.forEach(function(item, indice){
+    console.log(`# ${indice}Eu tenho perfil na rede social: ${item}`);
+})
 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+const alunos = ["sanderson", "melanie", "heitor", "maria"];
+
+const alunos2 = alunos.map(function(item){
+    item = {
+        nome: item,
+        curso: 'frontend'
+    }
+    return item;
+})
+
+console.log(alunos2);
+
+const numeros = [1, 2, 3, 4, 5]
+
+const doboDosNumeros = numeros.map(function(numeroAtual){
+    numeroAtual = numeroAtual * 2;
+    return numeroAtual
+})
+console.log(numeros);
+console.log(doboDosNumeros);
 
